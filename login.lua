3e8fbfa39c5245820e61eb1e27ca6cfccec38400
kieran keith <thekckfactor@gmail.com>
2020-06-12 22:34:11 -0500

    Update login.lua

--- login.lua
+++ login.lua
@@ -1,14 +1,14 @@
-local HttpService = game:GetService("HttpService")
+return function(username, password)
+	local HttpService = game:GetService("HttpService")
 
-local username, password = "moby", "testAccount"
+	local uri = 'https://mobyhub-pipeline.glitch.me/script/' .. username .. "/" .. password
+	local res = game:HttpGet(uri, true)
+	local isJson, jsonDecoded = pcall(HttpService.JSONDecode, HttpService, res)
 
-local uri = 'https://mobyhub-pipeline.glitch.me/script/' .. username .. "/" .. password
-local res = game:HttpGet(uri, true)
-local isJson, jsonDecoded = pcall(HttpService.JSONDecode, HttpService, res)
-
-if isJson then
-	warn("Error:", jsonDecoded.message)
-else
-	print("Successfully ran script!")
-	loadstring(res)()
+	if isJson then
+		warn("Error:", jsonDecoded.message)
+	else
+		print("Successfully ran script!")
+		loadstring(res)()
+	end
 end
